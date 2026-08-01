bc718bd71a61bcb2005edf4a89b6f6a4961842ec
Devansh Verma <devanshverma9417623955@gmail.com>
2026-08-01 15:04:51 +0530

    Time: 97 ms (16.11%) | Memory: 9.6 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-int solve(int i,int j,vector<int>&nums){
-    if (i>j){
-        return 0;
-    }
-    if(i==j){
-        return nums[i];
-    }
+        int solve(int i,int j,vector<int>&nums){
+            if(i>j){
+                return 0;
+            }
+            if (i==j){
+                return nums[i];
+            }
+            int take_i=nums[i]-solve(i+1,j,nums);
+            int take_j=nums[j]-solve(i,j-1,nums);
 
-        int take_i=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int take_j=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-        return max(take_i,take_j);
 
-}
-    bool predictTheWinner(vector<int>& nums) {
-        int total_score=0;
-        for (int i=0;i<nums.size();i++){
-            total_score+=nums[i];
+            return max(take_i,take_j);
         }
-        int player1_score= solve(0,nums.size()-1,nums);
-        int player2_score=total_score-player1_score;
 
-        int i=0;
-        int j= nums.size()-1;
-        return player1_score>=player2_score;
 
+
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        return solve(0,n-1,nums)>=0;
 
     }
 };

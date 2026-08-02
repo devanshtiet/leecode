10a59820343f56d2107675cc22bc7214016e46f7
Devansh Verma <devanshverma9417623955@gmail.com>
2026-08-02 18:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (81.48%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign=1;
+        for(int x:nums){
+            if(x<0){
+                sign=-sign;
+            }
+            if(x==0){
+                return 0;
+
+            }
+        }
+        return sign;
+    }
+};

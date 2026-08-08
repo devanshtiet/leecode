0daa6206c9c4035e8b4498b38287fb1d74a693e7
Devansh Verma <devanshverma9417623955@gmail.com>
2026-08-08 12:27:38 +0530

    Time: 1 ms (84.63%) | Memory: 12.5 MB (84.41%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,long long i,long long j ,long long k){
-        return nums[i]*100+nums[j]*10+nums[k];
-    }
-
     vector<int> findEvenNumbers(vector<int>& nums) {
-
         vector<int>result;
-        long long  n= nums.size();
-        for(long long i=0;i<n;i++){
-            for(long long j=0;j<n;j++){
-                for(long long k=0;k<n;k++){
-                    if(nums[i]!=0 && i!=j && i!=k && j!=k && nums[k]%2==0){
-                        result.push_back(helper(nums,i,j,k));
-                    }
-                }
-            }
+        int n=nums.size();
+        vector<int>mp(10,0);
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
         }
-         sort(result.begin(),result.end());
-         result.erase(unique(result.begin(),result.end()),result.end());
 
+        for (int i=1;i<=9;i++){
+                if(mp[i]==0){
+                    continue;
+                }
+                mp[i]--;
+                for(int j=0;j<=9;j++){
+                    if(mp[j]==0){
+                        continue;
+                    }
+                    mp[j]--;
+                    for (int k=0;k<9;k=k+2){
+                        if(mp[k]==0){
+                            continue;
+                        }
+                        result.push_back(i*100+j*10+k);
+                    }
+                    mp[j]++;
+
+                }
+                mp[i]++;
+
+        }
         return result;
     }
 };

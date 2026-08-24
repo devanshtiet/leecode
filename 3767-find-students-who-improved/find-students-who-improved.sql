19e87e14e7fb8e132962de78cdfd9f51a9d7963c
Devansh Verma <devanshverma9417623955@gmail.com>
2026-08-24 10:56:17 +0530

    Time: 416 ms (12.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,16 @@
+select s.student_id,s.subject,f.score as first_score,l.score as latest_score from Scores s 
+join Scores f
+on s.student_id=f.student_id
+and s.subject=f.subject
+and f.exam_date=(select min(exam_date) from Scores 
+where student_id=f.student_id  and subject=f.subject)
+join Scores l 
+on  s.student_id=l.student_id
+and s.subject=l.subject
+and l.exam_date=(select max(exam_date) from Scores 
+where student_id=l.student_id  and subject=l.subject)
+ 
+group by s.student_id ,s.subject,f.score,l.score
+having count(distinct s.exam_date)>=2
+and l.score>f.score
+order by student_id, subject;

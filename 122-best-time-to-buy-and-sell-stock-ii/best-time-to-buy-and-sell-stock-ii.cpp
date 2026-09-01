7e09cf8113b8b3660158e202906af50b4c83641c
Devansh Verma <devanshverma9417623955@gmail.com>
2026-09-01 16:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (65.14%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int profit=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                profit=profit+nums[i]-nums[i-1];
+            }
+        }
+        return profit;
+    }
+};

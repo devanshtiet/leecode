0060fb18026a09f6b7ddd7abf5df8f6a0e8692ad
Devansh Verma <devanshverma9417623955@gmail.com>
2026-09-02 13:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (63.76%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int result=0;
-        int far=0;
-        for(int h=0;h<=citations.size();h++){
-            int count=0;
-            for(int j=0;j<citations.size();j++){
-               if(citations[j]>=h){
-                count++;
-               }
-            }
-            if(count>=h){
-                result=h;
+        sort(citations.begin(),citations.end());
+        int n=citations.size();
+        for(int i=0;i<n;i++){
+            int h=n-i;
+            if(citations[i]>=h){
+                return h;
             }
         }
-        return result;
+        return 0;
     }
 };

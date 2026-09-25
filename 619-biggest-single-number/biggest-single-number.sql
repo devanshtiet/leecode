4d881cc9fb1847cb8c5fd2cdda2fb4468a833c3d
Devansh Verma <devanshverma9417623955@gmail.com>
2026-09-25 12:47:14 +0530

    Time: 419 ms (90.44%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
--- select max(num) from (select num from MyNumbers group by num having count(num)=1) as 
-
-select case when count(num)=1 then num else null END as num from MyNumbers group by num
-order by num desc limit 1;
+with temp as (
+    select num,count(num) as cnt from MyNumbers group by num
+)
+select max(num) as num from temp where cnt=1 order by num desc;
